c536c99a511edcf4cf2c3f8436ada2448581b0ab
Rucknium <Rucknium@protonmail.com>
2026-01-06 22:56:28 +0000

    Add ban list version 2

--- R/data.R
+++ R/data.R
@@ -20,14 +20,21 @@
 
 #' IP ban list of suspected spy nodes
 #'
-#' A set of suspected spy nodes. See this GitHub issue for more information:
+#' A set of suspected spy nodes. Ban list version 1 was released in December
+#' 2024. Ban list version 2 was released in Janraury 2026. See this GitHub
+#' issue for more information:
 #' \url{https://github.com/monero-project/meta/issues/1124}
 #'
-#' @format ## `ban_list`
+#' @format ## `ban_list_v1`
 #' A character vector with 423 elements. Most are singleton IP addresses. Six
 #' of them are IP addresses ranges in Classless Inter-Domain Routing (CIDR)
 #' notation.
+#' @format ## `ban_list_v2`
+#' A character vector with 431 elements.
 #'
 #' @source <https://github.com/Boog900/monero-ban-list>
-"ban_list"
+"ban_list_v1"
+
+#' @rdname ban_list_v1
+"ban_list_v2"
 
